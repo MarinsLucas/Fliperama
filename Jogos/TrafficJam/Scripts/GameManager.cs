cbde24cb61ad00e3a7e8332badcd12d6ffad29a3
Lucas Marins <lucasmarinsramalho@gmail.com>
2022-04-25 15:09:10 -0300

    Comentando o gamemanager.cs

--- Jogos/TrafficJam/Scripts/GameManager.cs
+++ Jogos/TrafficJam/Scripts/GameManager.cs
@@ -6,19 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Componentes")]
+    [Header("Componentes")] //paineis e texto de pontuação
     [SerializeField] Text scoreText; 
     [SerializeField] GameObject InGamePanel;
     [SerializeField] GameObject RestartPanel; 
 
-    [Header("Variáveis")]
+    [Header("Variáveis")] //Variáveis para dificuldade
     [SerializeField] float difficultInterval;
     float difficultTimer; 
     
     public bool isRunning; 
     float score;
 
-    public static GameManager instance;
+    public static GameManager instance; 
 
     void Awake()
     {
@@ -35,7 +35,7 @@ public class GameManager : MonoBehaviour
         InGamePanel.SetActive(true);
         RestartPanel.SetActive(false);
     }
-
+    //Função que define o fim do jogo
     public void GameOver()
     {
         isRunning = false;
@@ -44,17 +44,18 @@ public class GameManager : MonoBehaviour
         Debug.Log("O jogo terminou");
 
     }
-
+    //função para o botão de restart
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-
+    //função para o botão do mainmenu
     public void MainMenu()
     {
         SceneManager.LoadScene(0);
     }
-
+    //Atualiza pontuação
+    //Aumenta a dificuldade
     void Update()
     {
         if(isRunning)
